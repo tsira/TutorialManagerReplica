156160af315651829021f75129db1bc4ea69438a
Rambod Kermanizadeh <rambod@rambodMBP15.local>
2018-03-07 17:07:35 -0800

    Added new user and old user unit tests

--- Assets/TutorialManagerUnitTests.cs
+++ Assets/TutorialManagerUnitTests.cs
@@ -5,39 +5,34 @@ using NUnit.Framework;
 using System.Collections;
 using System.IO;
 
-public class TutorialManagerUnitTests : IPrebuildSetup {
+public class NewUserTests {
 
     GameObject testUtilGO;
     TutorialManagerWebHandlerTest testMono;
 
     bool requestReturned = false;
 
-	[Test]
-	public void TutorialManagerUnitTestsSimplePasses() {
-		// Use the Assert class to test conditions.
-	}
-
-    public void Setup()
-    {
-        testUtilGO = new GameObject();
-        testUtilGO.name = "TEST-GO";
-        testUtilGO.AddComponent<TutorialManagerWebHandlerTest>();
-
-        if(File.Exists(Application.persistentDataPath + "/Unity/" + Application.cloudProjectId + "/Analytics/values"))
-        {
-            File.Delete(Application.persistentDataPath + "/Unity/" + Application.cloudProjectId + "/Analytics/values");
-        }
-        PlayerPrefs.DeleteAll();
-        PlayerPrefs.Save();
-    }
-
 	[UnityTest]
-	public IEnumerator TutorialManagerUnitTestsWithEnumeratorPasses() {
+    [PrebuildSetup(typeof(TutorialManager_NewUserPrebuild))]
+	public IEnumerator CachedResponseMatchesServerResponse() {
         testUtilGO = GameObject.Find("TEST-GO");
         testMono = testUtilGO.GetComponent<TutorialManagerWebHandlerTest>();
         yield return new WaitUntil(() => testMono.IsTestFinished);
+        yield return new WaitUntil(() => PlayerPrefs.HasKey("adaptive_onboarding_show_tutorial"));
         Assert.AreEqual(PlayerPrefs.GetInt("adaptive_onboarding_show_tutorial"), testMono.toShow);
+        GameObject.Destroy(testUtilGO);
 	}
+}
+
+public class OldUserTests
+{
+    [UnityTest]
+    [PrebuildSetup(typeof(TutorialManager_OldUserPrebuild))]
+    public IEnumerator OldPlayer_ServerNoHit()
+    {
+        yield return new WaitUntil(() => Time.timeSinceLevelLoad > 10.0f);
+        Assert.IsFalse(PlayerPrefs.HasKey("adaptive_onboarding_show_tutorial"));
+    }
 }
 
 public class TutorialManagerWebHandlerTest : MonoBehaviour
@@ -57,19 +52,41 @@ public class TutorialManagerWebHandlerTest : MonoBehaviour
         //var deviceInfo = JsonUtility.FromJson<TutorialManager.DeviceInfo>(System.Text.Encoding.UTF8.GetString(request.uploadHandler.data));
         var response = JsonUtility.FromJson<TutorialManager.TutorialWebResponse>(request.downloadHandler.text);
         toShow = System.Convert.ToInt32(response.show_tutorial);
-        responseReturned = true;
-    }
-
-    private void Update()
-    {
-        if(responseReturned && PlayerPrefs.HasKey("adaptive_onboarding_show_tutorial"))
-        {
-            testFinished = true;
-        }
+        testFinished = true;
     }
 
     public bool IsTestFinished
     {
         get { return testFinished; }
+    }
+}
+
+public class TutorialManager_NewUserPrebuild : IPrebuildSetup
+{
+    public void Setup ()
+    {
+        var go = new GameObject();
+        go.name = "TEST-GO";
+        go.AddComponent<TutorialManagerWebHandlerTest>();
+
+        if (File.Exists(Application.persistentDataPath + "/Unity/" + Application.cloudProjectId + "/Analytics/values"))
+        {
+            File.Delete(Application.persistentDataPath + "/Unity/" + Application.cloudProjectId + "/Analytics/values");
+        }
+        PlayerPrefs.DeleteAll();
+        PlayerPrefs.Save();
+    }
+}
+
+public class TutorialManager_OldUserPrebuild : IPrebuildSetup
+{
+    public void Setup ()
+    {
+        if (!File.Exists(Application.persistentDataPath + "/Unity/" + Application.cloudProjectId + "/Analytics/values"))
+        {
+            File.Create(Application.persistentDataPath + "/Unity/" + Application.cloudProjectId + "/Analytics/values");
+        }
+        PlayerPrefs.DeleteAll();
+        PlayerPrefs.Save();
     }
 }
