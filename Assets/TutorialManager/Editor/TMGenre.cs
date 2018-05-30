9dc8b8a4393be25002091cb14c67167e1ec4222b
Marc Tanenbaum <marct@unity3d.com>
2018-05-29 23:45:10 -0700

    [UATM-175] Implemented TM window designs

--- Assets/TutorialManager/Editor/TMGenre.cs
+++ Assets/TutorialManager/Editor/TMGenre.cs
@@ -5,7 +5,7 @@ namespace UnityEngine.Analytics.TutorialManagerEditor
 {
     public class TMGenre
     {
-        static internal string[] genres = new string[] { "Action", "Adventure", "Arcade", "Board", "Card",
+        static internal string[] genres = new string[] { "Select...", "Action", "Adventure", "Arcade", "Board", "Card",
                 "Casino", "Casual", "Dice", "Educational", "Puzzle",
                 "Role playing", "Simulation", "Sports", "Strategy", "Trivia", "Word"};
         static internal int[] genreIds = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
