1424bb02ba99d947a1daf9d731f5e52bbe899c39
Marc Tanenbaum <marct@unity3d.com>
2018-05-29 23:45:10 -0700

    [UATM-175] Implemented TM window designs

--- Assets/TutorialManager/Editor/TutorialManagerWindow.cs
+++ Assets/TutorialManager/Editor/TutorialManagerWindow.cs
@@ -8,7 +8,7 @@ namespace UnityEngine.Analytics.TutorialManagerEditor
 {
     public class TutorialManagerWindow : EditorWindow
     {
-        private const string m_TabTitle = "Tutorial Manager";
+        private const string k_TabTitle = "Tutorial Manager";
 
         // Actual state
         private string m_AppId = "";
@@ -21,9 +21,10 @@ namespace UnityEngine.Analytics.TutorialManagerEditor
             }
         }
 
+        const string k_MustHaveAnalyticsMessage = "Unity Analytics is not enabled. This toll will not function without it. Please go to Window > Services and enable.";
         const string k_GenreTooltip = "Select the genre which best categorizes this game. Analytics uses this information to optimize your tutorial.";
         const string k_TutorialIdTooltip = "The name by which to identify this tutorial. Must be unique. Only alpha-numerics characters are allowed.";
-        const string k_StepIdTooltip = "The name by which to identify this step. Must be unique within this tutorial. Only alpha-numerics characters are allowed.";
+        const string k_StepIdTooltip = "The name by which to identify each step. Each name must be unique within a tutorial. Only alpha-numerics characters are allowed.";
         const string k_AddTutorialTooltip = "Add a new tutorial.";
         const string k_DeleteTutorialTooltip = "Delete this tutorial. WARNING: deleting a tutorial without cleaning up associated components can cause problems!";
         const string k_AddStepTooltip = "Add a step to this tutorial. You can have up to 10 steps per tutorial.";
@@ -33,41 +34,48 @@ namespace UnityEngine.Analytics.TutorialManagerEditor
         const string k_PushDataTooltip = "Upload local tutorial data to the server. This operation also informs the server that Tutorial Manager integration is complete.";
         const string k_TMTextTooLongMessage = "Some step text is too long; these fields will be truncated! Max text length is {0}. The following text should be shortened: {1}";
 
-        GUIContent genreGUIContent = new GUIContent("Genre", k_GenreTooltip);
-        GUIContent tutorialIdGUIContent = new GUIContent("ID", k_TutorialIdTooltip);
-        GUIContent stepIdGUIContent = new GUIContent("ID", k_StepIdTooltip);
-        GUIContent addStepButtonGUIContent = new GUIContent("+ Step", k_AddStepTooltip);
-        GUIContent addTutorialButtonGUIContent = new GUIContent("+ Tutorial", k_AddTutorialTooltip);
+        const int k_MaxSteps = 10;
+        const float k_ColumnWidth = 100f;
+
+        GUIContent genreGUIContent = new GUIContent("Game Genre", k_GenreTooltip);
+        GUIContent tutorialIdGUIContent = new GUIContent("Tutorial Key", k_TutorialIdTooltip);
+        GUIContent stepIdGUIContent = new GUIContent("Step Keys", k_StepIdTooltip);
+        GUIContent addStepButtonGUIContent = new GUIContent("+", k_AddStepTooltip);
+        GUIContent addTutorialButtonGUIContent = new GUIContent("Add Tutorial", k_AddTutorialTooltip);
         GUIContent deleteTutorialButtonGUIContent = new GUIContent("-", k_DeleteTutorialTooltip);
         GUIContent deleteStepButtonGUIContent = new GUIContent("-", k_DeleteStepTooltip);
         GUIContent goToDashboardButtonGUIContent = new GUIContent("Go to Dashboard", k_GoToDashboardTooltip);
         GUIContent pullButtonGUIContent = new GUIContent("Pull Data", k_PullDataTooltip);
         GUIContent pushButtonGUIContent = new GUIContent("Push Data", k_PushDataTooltip);
 
+        const string k_NeedsGenre = "Please select a genre before attempting to push.";
+
+        bool isGenreProblem;
+
         int genreId;
         string tutorialMarkedForDeletion;
+        GUIStyle addButtonStyle;
+        Vector2 m_ScrollPosition;
 
-
-        [MenuItem("Window/Unity Analytics/TutorialManager")]
+        [MenuItem("Window/Unity Analytics/Tutorial Manager")]
         static void TutorialManagerMenuOption()
         {
-            EditorWindow.GetWindow(typeof(TutorialManagerWindow), false, m_TabTitle);
+            EditorWindow.GetWindow(typeof(TutorialManagerWindow), false, k_TabTitle);
         }
 
         private void OnEnable()
         {
-
         }
 
         private void OnDisable()
         {
-
         }
 
         private void OnFocus()
         {
             RestoreAppId();
         }
+       
 
         protected void RestoreAppId()
         {
@@ -81,20 +89,32 @@ namespace UnityEngine.Analytics.TutorialManagerEditor
 
         private void OnGUI()
         {
+            if (!Analytics.enabled) {
+                EditorGUILayout.HelpBox(k_MustHaveAnalyticsMessage, MessageType.Warning, true);
+                return;
+            }
+            if (TMModel.TMData.tutorials.Count == 0) {
+                CreateTutorial();
+                return;
+            }
+            if (addButtonStyle == null) {
+                DefineStyles();
+            }
+
+            GUILayout.Space(5f);
+
             RenderHeader();
 
-            GUILayout.Space(10f);
-
+            m_ScrollPosition = EditorGUILayout.BeginScrollView(m_ScrollPosition);
             int tutorialCount = TMModel.TMData.tutorials.Count;
             for (int a = 0; a < tutorialCount; a++) {
 
                 var tutorial = TMModel.TMData.tutorials[a];
-                RenderTutorial(tutorial);
+                RenderTutorial(tutorial, a == 0);
             }
+            EditorGUILayout.EndScrollView();
 
-            GUILayout.Space(10f);
-
-            RenderWarnings();
+            GUILayout.Space(5f);
 
             RenderFooter();
 
@@ -102,11 +122,6 @@ namespace UnityEngine.Analytics.TutorialManagerEditor
             {
                 DestroyTutorial(tutorialMarkedForDeletion);
                 tutorialMarkedForDeletion = string.Empty;
-            }
-
-            if(GUILayout.Button("Write"))
-            {
-                PushData();
             }
         }
 
@@ -151,10 +166,15 @@ namespace UnityEngine.Analytics.TutorialManagerEditor
                                                TutorialManagerModelMiddleware.k_MaxTextlength, stringiFiedList);
                 EditorGUILayout.HelpBox(message, MessageType.Warning, true);
             }
+
+            if (isGenreProblem) {
+                EditorGUILayout.HelpBox(k_NeedsGenre, MessageType.Warning, true);
+            }
         }
 
         private void RenderFooter()
         {
+            RenderWarnings();
             using (new GUILayout.HorizontalScope()) {
                 if (GUILayout.Button(addTutorialButtonGUIContent)) {
                     CreateTutorial();
@@ -167,19 +187,11 @@ namespace UnityEngine.Analytics.TutorialManagerEditor
                 }
                 RenderDashboardLink();
             }
-
-            if (GUILayout.Button("Test Auth")) {
-                PullData();
-            }
-
-            if (GUILayout.Button("Clear Model")) {
-                TMModel.Clear();
-            }
         }
 
         private void RenderGenre()
         {
-            EditorGUILayout.LabelField(genreGUIContent, GUILayout.Width(40f));
+            EditorGUILayout.LabelField(genreGUIContent, EditorStyles.miniLabel, GUILayout.Width(k_ColumnWidth));
             int id = genreId;
             id = EditorGUILayout.IntPopup(id, TMGenre.genres, TMGenre.genreIds);
 
@@ -197,27 +209,33 @@ namespace UnityEngine.Analytics.TutorialManagerEditor
             }
         }
 
-        private void RenderTutorial(TutorialEntity tutorial)
+        private void RenderTutorial(TutorialEntity tutorial, bool deleteDisabled)
         {
-            using (new GUILayout.HorizontalScope()) {
-                EditorGUILayout.LabelField(tutorialIdGUIContent, GUILayout.Width(40f));
-                RestrictInputCharacters();
-                string tutorialId = EditorGUILayout.TextField(tutorial.id);
-                if (tutorialId != tutorial.id) {
-                    UpdateTutorial(tutorial.id, tutorialId);
-                }
-                if (GUILayout.Button(deleteTutorialButtonGUIContent, GUILayout.Width(20f))) {
-                    MarkTutorialForDeletion(tutorialId);
-                }
-                if (GUILayout.Button(addStepButtonGUIContent)) {
-                    CreateStep(tutorial.id);
-                }
-            }
+            using (new GUILayout.VerticalScope("box")) {
+                using (new GUILayout.HorizontalScope()) {
+                    EditorGUILayout.LabelField(tutorialIdGUIContent, EditorStyles.miniLabel, GUILayout.Width(k_ColumnWidth));
 
-            for (int a = 0; a < tutorial.steps.Count; a++) {
-                string stepId = tutorial.steps[a];
-                if (TMModel.TMData.stepTable.ContainsKey(stepId)) {
-                    RenderStep(tutorial, a, TMModel.TMData.stepTable[stepId]);
+                    RestrictInputCharacters();
+                    string tutorialId = EditorGUILayout.TextField(tutorial.id);
+                    if (tutorialId != tutorial.id) {
+                        UpdateTutorial(tutorial.id, tutorialId);
+                    }
+
+                    var prevColor = GUI.backgroundColor;
+                    GUI.backgroundColor = Color.clear;
+                    EditorGUI.BeginDisabledGroup(deleteDisabled);
+                    if (GUILayout.Button(deleteTutorialButtonGUIContent, addButtonStyle)) {
+                        MarkTutorialForDeletion(tutorialId);
+                    }
+                    EditorGUI.EndDisabledGroup();
+                    GUI.backgroundColor = prevColor;
+                }
+
+                for (int a = 0; a < tutorial.steps.Count; a++) {
+                    string stepId = tutorial.steps[a];
+                    if (TMModel.TMData.stepTable.ContainsKey(stepId)) {
+                        RenderStep(tutorial, a, TMModel.TMData.stepTable[stepId]);
+                    }
                 }
             }
         }
@@ -227,17 +245,34 @@ namespace UnityEngine.Analytics.TutorialManagerEditor
             using (new GUILayout.HorizontalScope()) {
                 string displayName = ParseDisplayNameFromStep(tutorial.id, step.id);
                 string newDisplayName = displayName;
-                GUIContent gUIContent = stepIdGUIContent;
-                gUIContent.text = (index + 1).ToString();
-                EditorGUILayout.LabelField(gUIContent, GUILayout.Width(40f));
+                if (index == 0) {
+                    EditorGUILayout.LabelField(stepIdGUIContent, EditorStyles.miniLabel, GUILayout.Width(k_ColumnWidth));
+                } else {
+                    EditorGUILayout.LabelField("", GUILayout.Width(k_ColumnWidth));
+                }
                 RestrictInputCharacters();
                 newDisplayName = EditorGUILayout.TextField(displayName);
                 if (newDisplayName != displayName) {
                     UpdateStep(step.id, ConstructStepIdFromDisplayName(tutorial.id, newDisplayName));
                 }
+                if (index == tutorial.steps.Count - 1) {
+                    bool plusEnabled = index < k_MaxSteps - 1;
+                    bool minusEnabled = index > 0;
 
-                if (GUILayout.Button(deleteStepButtonGUIContent, GUILayout.Width(20f))) {
-                    DestroyStep(step.id);
+                    var prevColor = GUI.backgroundColor;
+                    GUI.backgroundColor = Color.clear;
+                    EditorGUI.BeginDisabledGroup(!plusEnabled);
+
+                    if (GUILayout.Button(addStepButtonGUIContent, addButtonStyle)) {
+                        CreateStep(tutorial.id);
+                    }
+                    EditorGUI.EndDisabledGroup();
+                    EditorGUI.BeginDisabledGroup(!minusEnabled);
+                    if (GUILayout.Button(deleteStepButtonGUIContent, addButtonStyle)) {
+                        DestroyStep(step.id);
+                    }
+                    EditorGUI.EndDisabledGroup();
+                    GUI.backgroundColor = prevColor;
                 }
             }
         }
@@ -275,6 +310,9 @@ namespace UnityEngine.Analytics.TutorialManagerEditor
         {
             genreId = id;
             TMModel.SaveGenre(TMGenre.genres[id]);
+            if (id != 0) {
+                isGenreProblem = false;
+            }
         }
 
         private void CreateTutorial()
@@ -291,6 +329,7 @@ namespace UnityEngine.Analytics.TutorialManagerEditor
                 }
             }
             TMModel.CreateTutorialEntity(id);
+            CreateStep(id);
         }
 
         private void UpdateTutorial(string oldId, string newId)
@@ -340,8 +379,12 @@ namespace UnityEngine.Analytics.TutorialManagerEditor
 
         private void PushData()
         {
-            TutorialManagerEditorWebHandler.TMRSWriteResponseReceived += WriteResponseReceived;
-            m_WebRequestEnumerator = TutorialManagerEditorWebHandler.Write(m_AppId, TMModel.TMData);
+            if (genreId == 0) {
+                isGenreProblem = true;
+            } else {
+                TutorialManagerEditorWebHandler.TMRSWriteResponseReceived += WriteResponseReceived;
+                m_WebRequestEnumerator = TutorialManagerEditorWebHandler.Write(m_AppId, TMModel.TMData);
+            }
         }
 
         void WriteResponseReceived(bool succes)
@@ -372,6 +415,14 @@ namespace UnityEngine.Analytics.TutorialManagerEditor
             //Temp logging to ensure it worked
             Debug.Log(remoteSettings.Count);
             Debug.Log(remoteSettings.First().key);
+        }
+
+        private void DefineStyles()
+        {
+            addButtonStyle = new GUIStyle(GUI.skin.button);
+            addButtonStyle.normal.background = null;
+            addButtonStyle.fontStyle = FontStyle.Bold;
+            addButtonStyle.fixedWidth = 20f;
         }
     }
 }
