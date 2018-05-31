4762934a3b81ed209db74fe33db8dcaeaa46879c
Marc Tanenbaum <marct@unity3d.com>
2018-05-30 19:25:43 -0700

    [UATM-175] Adaptive Content styling. Still a couple visual bugs.

--- Assets/TutorialManager/Editor/Inspectors/AdaptiveContentInspector.cs
+++ Assets/TutorialManager/Editor/Inspectors/AdaptiveContentInspector.cs
@@ -1,6 +1,7 @@
 using UnityEditor;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace UnityEngine.Analytics.TutorialManagerRuntime
 {
@@ -8,74 +9,42 @@ namespace UnityEngine.Analytics.TutorialManagerRuntime
     public class AdaptiveContentInspector : Editor
     {
 
-        GUIContent bindingLabel = new GUIContent("Binding");
-
+        GUIContent bindingLabel = new GUIContent("Binding ID", "The tutorial and step ID to which this component will be bound.");
+        GUIContent blankBinding = new GUIContent("Select a binding...");
         const string k_TMSettingsRequiredMessage = "Adaptive content keys must be set up in the Tutorial Manager window.\nGo to Window > Unity Analytics > TutorialManager";
 
-        int bindingIndex = 0;
-        int lastKnownBindingIndex = -1;
-        GUIContent[] bindingContent;
-
-
+        GenericMenu bindingsMenu;
+        int bindingIndex;
         List<string> stepIds = new List<string>();
 
         public override void OnInspectorGUI()
         {
             AdaptiveContent myTarget = (AdaptiveContent)target;
-
             TutorialManagerModel model = TutorialManagerModelMiddleware.GetInstance().TMData;
-
-            if (CachedStepsAreStillValid(model) == false) {
-                ConstructBindings(model);
-            }
-
-            if (bindingContent.Count() == 0) {
+            if (model.steps.Count() == 0) {
                 // Display warning
                 EditorGUILayout.HelpBox(k_TMSettingsRequiredMessage, MessageType.Warning, true);
 
             } else {
-                // Display popup
-                bindingIndex = stepIds.IndexOf(myTarget.bindingId);
-                bindingIndex = EditorGUILayout.Popup(bindingLabel, bindingIndex, bindingContent);
+                bindingIndex = GetCurrentIndex(model, myTarget);
+                RenderBindingPopup(myTarget);
             }
-
-            // If changed, make the new connection
-            if (bindingIndex != lastKnownBindingIndex && bindingIndex > -1) {
-                BindTo(myTarget, stepIds[bindingIndex]);
-            }
-
-            lastKnownBindingIndex = bindingIndex;
         }
 
-
-        protected void ConstructBindings(TutorialManagerModel manifest)
+        static int GetCurrentIndex(TutorialManagerModel model, AdaptiveContent myTarget)
         {
-            stepIds = new List<string>();
-            bindingContent = new GUIContent[manifest.steps.Count];
-
-            int a = 0;
-            manifest.stepTable.ToList().ForEach(x => {
-                var stepId = x.Value.id;
-                stepIds.Add(stepId);
-                // FIXME : Do we need this? Might lose, depending on Sean's design. -- MAT, May 17, 2018
-                //var tutorialName = FindTutorialByStepId(manifest.tutorialTable, stepId);
-                var contentString = string.Format("{0}", stepId);
-                bindingContent[a] = new GUIContent(contentString);
-                a++;
-            });
+            return model.steps.Select((value, index) => new { value, index })
+                        .Where(pair => pair.value.id == myTarget.bindingId)
+                        .Select(pair => pair.index + 1)
+                        .FirstOrDefault() - 1;
         }
 
-        static string FindTutorialByStepId(Dictionary<string, TutorialEntity> table, string stepId)
+        private bool CachedStepsAreStillValid(TutorialManagerModel model)
         {
-            return table.Where(x => x.Value.steps.Contains(stepId)).First().Value.id;
-        }
-
-        private bool CachedStepsAreStillValid(TutorialManagerModel manifest)
-        {
-            bool isValid = manifest.stepTable.Count == stepIds.Count;
+            bool isValid = model.stepTable.Count == stepIds.Count;
             if (isValid) {
                 int aa = 0;
-                foreach (KeyValuePair<string, StepEntity> t in manifest.stepTable) {
+                foreach (KeyValuePair<string, StepEntity> t in model.stepTable) {
                     if (t.Value.id != stepIds[aa]) {
                         isValid = false;
                         break;
@@ -86,9 +55,58 @@ namespace UnityEngine.Analytics.TutorialManagerRuntime
             return isValid;
         }
 
-        protected void BindTo(AdaptiveContent myTarget, string id)
+        protected void BindTo(AdaptiveContent myTarget, string id, int index)
         {
+            bindingIndex = index;
             myTarget.bindingId = id;
+        }
+
+        protected void RenderBindingPopup(AdaptiveContent myTarget)
+        {
+            TutorialManagerModel model = TutorialManagerModelMiddleware.GetInstance().TMData;
+            using (new GUILayout.HorizontalScope()) {
+                var labelRect = EditorGUILayout.GetControlRect();
+                EditorGUI.LabelField(labelRect, bindingLabel);
+                var buttonRect = EditorGUILayout.GetControlRect();
+                string id = model.steps[bindingIndex].id;
+                string bindingDisplayName = Regex.Replace(id, "-", ", ");
+                if (GUI.Button(buttonRect, new GUIContent(bindingIndex < 0 ? blankBinding : new GUIContent(bindingDisplayName)), EditorStyles.popup)) {
+                    BuildBindingsMenu(bindingIndex);
+                    bindingsMenu.DropDown(buttonRect);
+                }
+            }
+        }
+
+        void BuildBindingsMenu(int selectedIndex = -1)         {
+            TutorialManagerModel model = TutorialManagerModelMiddleware.GetInstance().TMData;             bindingsMenu = new GenericMenu();
+            int index = 0;
+            model.steps.ForEach(step => {
+                string stepId = step.id;
+                bindingsMenu.AddItem(
+                    new GUIContent(stepId.Replace('-', '/')),
+                    index == selectedIndex,
+                    SetSelectedBinding,
+                    new SelectedBindingInfo(stepId, index)
+                );
+                index++;
+            });         }
+
+        void SetSelectedBinding(object info)
+        {
+            var selectedBindingInfo = (SelectedBindingInfo)info;
+            BindTo(target as AdaptiveContent, selectedBindingInfo.bindingId, selectedBindingInfo.bindingIndex);
+        }
+    }
+
+    struct SelectedBindingInfo
+    {
+        public string bindingId;
+        public int bindingIndex;
+
+        public SelectedBindingInfo(string stepId, int index)
+        {
+            bindingId = stepId;
+            bindingIndex = index;
         }
     }
 }
